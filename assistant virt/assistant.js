e28091d2cc650bd2bf721ba2790b0ff2d7cefa3d
Johsan16 <211240@umons.ac.be>
2024-04-20 22:41:56 +0200

    debut tuto code

--- assistant virt/assistant.js
+++ assistant virt/assistant.js
@@ -17,16 +17,12 @@ print("Je vais bien")\n\
         indiceCount++;
         console.log(indiceCount);
       } else {
-        alert('Pas plus de trois indices disponibles.');
+        alert('Pas plus de '+indices.length+' indices disponibles.');
       }
     });
 
 
     const inputWords = [
-"\n\
-",
-"\n\
-",
 "\n\
 "
      ];
@@ -34,7 +30,7 @@ print("Je vais bien")\n\
     const inputText = document.getElementById('inputText');
    
     const outputWords = [
-'Je vais bien', '', ''
+'Je vais bien'
     ];
     let currentIndex = 0; // Indice du mot actuellement affiché
   
@@ -87,16 +83,38 @@ function getQueryParams() {
     alert('La valeur de "id" est : ' + idValue);
     // Vous pouvez utiliser idValue pour effectuer des actions spéciales en fonction de la valeur de "id"
   }
-  
 
-
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('mouseenter', function() {
-        const description = this.getAttribute('data-assistant'); // Assurez-vous d'ajouter cet attribut à vos boutons
-        document.getElementById('texte-description').textContent = description;
-    });
-
-    button.addEventListener('mouseleave', function() {
-        document.getElementById('texte-description').textContent = 'Cliquez sur un bouton pour obtenir des informations.';
-    });
-});
+let indiceDial=0;
+  document.getElementById('btn-assistant').addEventListener('click', function() {
+    const dialogue = [
+      "Il semblerait que tu aies perdu la memoire alors laisse moi te reexpliquer comment ca fonctionne",
+      "Ici sera où ta magie prendra forme. C'est ici que tu vas coder",
+      "\"Mais comment savoir ce que je dois coder ?\" me diras-tu",
+      "Et bien pour le savoir, tu devras cliquer sur ce bouton",
+      "non",
+      "non2",
+      "non3"
+           ];
+    if (indiceDial===1){
+      document.getElementById('editor').style="height: 500px;border-radius: 10px; box-shadow: 0 0 0 1vh rgba(255, 0, 0, 0);animation: blinker 2.5s linear infinite;";
+      document.getElementById('assistant-virtuel').style.boxShadow="0 0 0 max(100vh, 100vw) rgba(0, 0, 0, 0)";
+    }
+    if (indiceDial===3){
+      document.getElementById('btn-assistant').style.visibility="hidden";
+      document.getElementById('editor').style="height: 500px;border-radius: 10px;";
+      document.getElementById('btn_info_sup').style.boxShadow="0 0 0 1vh rgba(255, 0, 0, 0)";
+      document.getElementById('btn_info_sup').style.animation="blinker 2.5s linear infinite";
+      document.getElementById('btn_info_sup').addEventListener('click',function(){
+        document.getElementById('assistant-dialogue').innerText = dialogue[indiceDial] ;
+        indiceDial++;
+        document.getElementById('btn-assistant').style.visibility="visible";
+      },{once:true});
+    }
+    if (indiceDial===4){}
+    if (indiceDial===5){}
+    if (indiceDial===6){}
+    if (indiceDial < dialogue.length) {
+        document.getElementById('assistant-dialogue').innerText = dialogue[indiceDial] ;
+        indiceDial++;
+      }
+  });
